1e2304537087b3d5f614cb0acb241d808753a150
wiktorks <wiktorks1994@gmail.com>
2019-02-19 18:14:27 +0100

    Dodanie menu rozwijanego z typami plików, poprawa paska miejsca i wyświetlanie dostępnego miejsca w side-bar.

--- routes/users.js
+++ routes/users.js
@@ -13,22 +13,38 @@ var db = mysql.createConnection({
 /* GET users listing. */
 router.get('/:userId', function(req, res, next) {
   var userId = req.params.userId;
+  var fileTypes = 'SELECT DISTINCT filetype FROM files WHERE fk_userid = ' + userId + ' AND trash = FALSE ORDER BY CHAR_LENGTH(filename)';
   var dataQuery = 'SELECT idfiles, filename, filetype, added, filesize FROM files WHERE fk_userid = ' + userId + ' AND trash = FALSE ORDER BY added DESC';
-  var spacetaken = 'SELECT spacetaken FROM users WHERE iduser = ' + userId;
-  //console.log(sql);
-  db.query(dataQuery, function(error, data) {
-    console.log(data);
-    if(data.length > 0){
-        res.render('user', {
-          title: 'Repozytorium',
-          dane: data,
-          partial: 'part_tableData'
+  var spacetaken = 'SELECT spacetaken FROM user WHERE iduser = ' + userId;
+
+  db.query(spacetaken, function(error, data) {
+    spacetaken = data[0].spacetaken;
+    if(spacetaken > 0){
+      db.query(dataQuery, function(error, data) {
+        var mainData = data;
+        db.query(fileTypes, function(error, data) {
+          res.render('user', {
+            title: 'Repozytorium',
+            userId: userId,
+            dane: {mainData: mainData, fileData: data},
+            partial: 'part_tableData',
+            spacetaken: spacetaken
+          });
         });
+      });
     } else {
         res.render('user', {
           title: 'Repozytorium',
-          dane: '',
-          partial: 'part_missingData'
+          userId: userId,
+          dane: { mainData: {
+            url: 'https://image.freepik.com/free-vector/empty-cardboard-box-opened_34950-8.jpg',
+            upperSub: 'Twoje repozytorium jest puste',
+            lowerSub: 'Dodaj nowe pliki'
+            },
+            fileData: ''
+          },
+          partial: 'part_missingData',
+          spacetaken: spacetaken
         });
     }
 
@@ -38,6 +54,8 @@ router.get('/:userId', function(req, res, next) {
 router.get('/:userId/favourites', function(req, res, next) {
   var userId = req.params.userId;
   var dataQuery = 'SELECT idfiles, filename, filetype, added, filesize FROM favourites WHERE fk_userid = ' + userId;
+  var spacetaken = 'SELECT spacetaken FROM users WHERE iduser = ' + userId;
+
 });
 
 module.exports = router;
