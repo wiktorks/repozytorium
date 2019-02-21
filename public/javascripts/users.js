b21a1781099b0b21e562e75ed22547d18c781c23
wiktorks <wiktorks1994@gmail.com>
2019-02-21 11:50:53 +0100

    Wersja do oddania

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -1,28 +1,102 @@
-function extend(){
+function extend() {
     let nav = document.getElementById("menu");
     let article = document.getElementById("content");
     nav.classList.toggle("extended");
     article.classList.toggle("extended");
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("#menu-sub").hide();
-    $("#drop").click(function(){
+    $("#drop").click(function () {
         menu = document.getElementById("menu");
-        if(menu.classList.contains("extended")) {
+        if (menu.classList.contains("extended")) {
             $("#menu-sub").slideToggle();
         }
     });
-  });
+});
 
-function toTrash(idFile){
-    alert("idFile do śmieci: " + idFile);
+function getProperName(name) {
+    if (name === 'Repozytorium') {
+        return '';
+    } else {
+        return 'favourites';
+    }
 }
 
-function toDownload(idFile) {
-    alert("idFile do pobrania: " + idFile);
+function toTrash(data) {
+    data.title = getProperName(data.title);
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 402) {
+                alert(xhttp.response);
+            } else if (xhttp.status === 200) {
+                let json = JSON.parse(xhttp.response)
+                window.location.replace(json.url);
+            }
+        }
+    }
+    var url = '/users/' + data.userId + '/trash';
+    xhttp.open('POST', url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+    xhttp.send(JSON.stringify(data));
 }
 
-function toFavourite(idFile) {
-    alert("idFile do ulubionych: " + idFile);
+function toDownload(idFile, idUser) {
+    alert("idFile do pobrania: " + idFile + ", user: " + idUser);
+}
+
+function toFavourite(data) {
+    data.title = getProperName(data.title);
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 402) {
+                alert(xhttp.response);
+            } else if (xhttp.status === 200) {
+                let json = JSON.parse(xhttp.response)
+                window.location.replace(json.url);
+            }
+        }
+    }
+    var url = '/users/' + data.userId + '/favourites';
+    xhttp.open('POST', url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+    xhttp.send(JSON.stringify(data));
+}
+
+function toRecover(data) {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 402) {
+                alert(xhttp.response);
+            } else if (xhttp.status === 200) {
+                let json = JSON.parse(xhttp.response)
+                window.location.replace(json.url);
+            }
+        }
+    }
+    var url = '/users/' + data.userId + '/trash/recover';
+    xhttp.open('POST', url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+    xhttp.send(JSON.stringify(data));
+}
+
+function toDelete(data) {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 402) {
+                alert(xhttp.response);
+            } else if (xhttp.status === 200) {
+                let json = JSON.parse(xhttp.response)
+                window.location.replace(json.url);
+            }
+        }
+    }
+    var url = '/users/' + data.userId + '/trash/delete';
+    xhttp.open('POST', url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+    xhttp.send(JSON.stringify(data));
 }
--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ var db = mysql.createConnection({
 router.get('/:userId', function(req, res, next) {
   var userId = req.params.userId;
   var fileTypes = 'SELECT DISTINCT filetype FROM files WHERE fk_userid = ' + userId + ' AND trash = FALSE ORDER BY CHAR_LENGTH(filename)';
-  var dataQuery = 'SELECT idfiles, filename, filetype, added, filesize FROM files WHERE fk_userid = ' + userId + ' AND trash = FALSE ORDER BY added DESC';
+  var dataQuery = 'SELECT idfiles, filename, filetype, added, filesize, favourite FROM files WHERE fk_userid = ' + userId + ' AND trash = FALSE ORDER BY added DESC';
   var spacetaken = 'SELECT spacetaken FROM user WHERE iduser = ' + userId;
 
   db.query(spacetaken, function(error, data) {
@@ -51,11 +51,137 @@ router.get('/:userId', function(req, res, next) {
   });
 });
 
-router.get('/:userId/favourites', function(req, res, next) {
+router.get('/:userId/favourites', function (req, res, next) {
   var userId = req.params.userId;
-  var dataQuery = 'SELECT idfiles, filename, filetype, added, filesize FROM favourites WHERE fk_userid = ' + userId;
-  var spacetaken = 'SELECT spacetaken FROM users WHERE iduser = ' + userId;
+  var fileTypes = 'SELECT DISTINCT filetype FROM files WHERE fk_userid = ' + userId + ' AND trash = FALSE ORDER BY CHAR_LENGTH(filename)';
+  var dataQuery = 'SELECT idfiles, filename, filetype, added, filesize, favourite FROM favourites WHERE fk_userid = ' + userId + ' ORDER BY added DESC';
+  var spacetaken = 'SELECT spacetaken FROM user WHERE iduser = ' + userId;
 
+  db.query(spacetaken, function (error, data) {
+    spacetaken = data[0].spacetaken;
+    db.query(dataQuery, function (error, data) {
+      var mainData = data;
+      if (mainData.length > 0) {
+        db.query(fileTypes, function (error, data) {
+          res.render('user', {
+            title: 'Ulubione',
+            userId: userId,
+            dane: {mainData: mainData, fileData: data},
+            partial: 'part_tableData',
+            spacetaken: spacetaken
+          });
+        });
+      } else {
+        res.render('user', {
+          title: 'Ulubione',
+          userId: userId,
+          dane: {
+            mainData: {
+              url: 'https://ih0.redbubble.net/image.439563915.3616/ap,550x550,16x12,1,transparent,t.png',
+              upperSub: 'Aktualnie nie masz żadnych ulubionych',
+              lowerSub: 'Dodaj nowe pliki'
+            },
+            fileData: ''
+          },
+          partial: 'part_missingData',
+          spacetaken: spacetaken
+        });
+      }
+    });
+  });
+});
+
+router.post('/:userId/favourites', function (req, res, next) {
+  var sql = '';
+  console.log('Ulubione: ' + req.body.fav);
+  if(req.body.fav === 1){
+    sql = 'UPDATE files SET favourite = 0 WHERE idfiles = ' + req.body.idfiles;
+  }
+  else {
+    sql = 'UPDATE files SET favourite = 1 WHERE idfiles = ' + req.body.idfiles;
+  }
+  db.query(sql, function(error, data) {
+    res.status(200).send({url: '/users/' + req.body.userId + '/' + req.body.title});
+  });
+});
+
+router.get('/:userId/trash', function (req, res, next) {
+  var userId = req.params.userId;
+  var fileTypes = 'SELECT DISTINCT filetype FROM files WHERE fk_userid = ' + userId + ' AND trash = FALSE ORDER BY CHAR_LENGTH(filename)';
+  var dataQuery = 'SELECT idfiles, filename, filetype, added, filesize FROM trash WHERE fk_userid = ' + userId + ' ORDER BY added DESC';
+  var spacetaken = 'SELECT spacetaken FROM user WHERE iduser = ' + userId;
+
+  db.query(spacetaken, function (error, data) {
+    spacetaken = data[0].spacetaken;
+    db.query(dataQuery, function (error, data) {
+      var mainData = data;
+      if (mainData.length > 0) {
+        db.query(fileTypes, function (error, data) {
+          res.render('user', {
+            title: 'Kosz',
+            userId: userId,
+            dane: {mainData: mainData, fileData: data},
+            partial: 'part_tableData',
+            spacetaken: spacetaken
+          });
+        });
+      } else {
+        res.render('user', {
+          title: 'Kosz',
+          userId: userId,
+          dane: {
+            mainData: {
+              url: 'http://icons.iconarchive.com/icons/yellowicon/flat/256/Empty-Trash-icon.png',
+              upperSub: 'Kosz jest pusty',
+              lowerSub: ''
+            },
+            fileData: ''
+          },
+          partial: 'part_missingData',
+          spacetaken: spacetaken
+        });
+      }
+    });
+  });
+});
+
+router.post('/:userId/trash', function (req, res, next) {
+  var sql = 'UPDATE files SET trash = 1 WHERE idfiles = ' + req.body.idfiles;
+
+  db.query(sql, function(error, data) {
+    res.status(200).send({url: '/users/' + req.body.userId + '/' + req.body.title});
+  });
+});
+
+router.post('/:userId/trash/recover', function(req, res, next) {
+  var sql = 'UPDATE files SET trash = 0 WHERE idfiles = ' + req.body.idfiles;
+
+  db.query(sql, function(error, data) {
+    res.status(200).send({url: '/users/' + req.body.userId + '/trash'});
+  });
+});
+
+router.post('/:userId/trash/delete', function(req, res, next) {
+  var sql = 'DELETE FROM files WHERE idfiles = ' + req.body.idfiles;
+
+  db.query(sql, function(error, data) {
+    res.status(200).send({url: '/users/' + req.body.userId + '/trash'});
+  });
+});
+
+router.post('/:userId/add', function(req, res, next) {
+  userId = req.params.userId;
+  var sql = 'INSERT INTO files (filename, filetype, added, filesize, fk_userid) VALUES ?';
+  var date;
+  date = new Date();
+  date = date.getUTCFullYear() + '-' +
+      ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
+      ('00' + date.getUTCDate()).slice(-2)
+  console.log(sql + ', date: ' + date);
+  var values = [[req.body.filename, req.body.filetype, date, req.body.filesize, userId]];
+  db.query(sql, [values], function(error, data) {
+    res.redirect('/users/'+userId);
+  });
 });
 
 module.exports = router;
